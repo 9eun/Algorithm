bc93227254325190c65309b7dfcbf26dd21a6ffd
9eun <113655865+9eun@users.noreply.github.com>
2023-12-21 15:49:07 +0900

    [Gold V] Title: 치킨 배달, Time: 348 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+
+n,m = map(int,input().split())
+chicken=[]
+house=[]
+
+for i in range(n):
+  arr = list(map(int,input().split()))
+  for c in range(n):
+    if arr[c]==1:
+      house.append((i,c))
+    elif arr[c]==2:
+      chicken.append((i,c))
+combv = list(combinations(chicken,m))
+
+def sum(combv):
+  result = 0
+  for hx,hy in house:
+    temp = 1e9
+    for cx, cy in combv:
+      temp = min(temp,abs(hx-cx)+abs(hy-cy))
+    result+=temp
+  return result
+
+result = 1e9
+for com in combv:
+  result = min(result, sum(com))
+print(result)
