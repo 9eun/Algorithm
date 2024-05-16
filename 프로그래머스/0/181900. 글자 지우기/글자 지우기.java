6bdf732ab57684a64b063601dff83f77f9058467
9eun <113655865+9eun@users.noreply.github.com>
2024-05-16 19:04:27 +0900

    [level 0] Title: 글자 지우기, Time: 1.67 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -3,15 +3,9 @@ import java.util.*;
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = "";
-        StringBuilder sb = new StringBuilder(my_string);
-        Integer[] arr = Arrays.stream(indices).boxed().toArray(Integer[]::new);
-        Arrays.sort(arr,Collections.reverseOrder());
-
-        System.out.println(Arrays.toString(arr));
-        for(int i : arr){
-            sb.deleteCharAt(i);    
-        }
-        
-        return sb.toString();
+        String[] arr = my_string.split("");
+        for(int i=0;i<indices.length;i++) arr[indices[i]] = "";
+        for(String x : arr) answer+=x;
+        return answer;
     }
 }
