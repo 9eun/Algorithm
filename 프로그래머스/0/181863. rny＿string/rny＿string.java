c5611f00630a5149a920a78d23953d3df27f5686
9eun <113655865+9eun@users.noreply.github.com>
2024-06-11 02:09:47 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        String str = rny_string.replace("m","rn");
+        
+        return str;
+    }
+}
