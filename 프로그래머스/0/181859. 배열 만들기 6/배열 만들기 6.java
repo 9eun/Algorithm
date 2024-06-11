f84d7e583db08208569428e07a697baed9834003
9eun <113655865+9eun@users.noreply.github.com>
2024-06-11 12:11:17 +0900

    [level 0] Title: 배열 만들기 6, Time: 20.19 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer>stk = new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            if(stk.isEmpty())stk.push(arr[i]);
+            else if(stk.peek()==arr[i])stk.pop();
+            else stk.push(arr[i]);
+        }
+        if(stk.isEmpty())stk.push(-1);
+        int[]answer = stk.stream().mapToInt(i->i).toArray();
+        return answer;
+    }
+}
