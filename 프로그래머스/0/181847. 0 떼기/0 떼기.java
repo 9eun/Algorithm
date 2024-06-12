b71ab5deed9ba13491e818f2ba374c3204820e3a
9eun <113655865+9eun@users.noreply.github.com>
2024-06-13 02:05:05 +0900

    [level 0] Title: 0 떼기, Time: 6.28 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int a = Integer.parseInt(n_str);
+        
+        answer = a+"";
+        return answer;
+        
+    }
+}
