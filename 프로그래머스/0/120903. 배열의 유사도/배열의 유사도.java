8cbb18b2051ef8413345ef3e3e8609146cc09209
9eun <113655865+9eun@users.noreply.github.com>
2024-07-02 13:52:18 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for(String s : s1){
+            for(String ss : s2){
+                if(s.equals(ss)) answer+=1;
+            }
+        }
+        return answer;
+    }
+}
