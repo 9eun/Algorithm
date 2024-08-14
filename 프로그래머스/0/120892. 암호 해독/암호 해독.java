63f2da358a935a2e86cd3f4b03e5f8cfe8be0ac8
9eun <113655865+9eun@users.noreply.github.com>
2024-08-14 19:37:34 +0900

    [level 0] Title: 암호 해독, Time: 5.60 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        String[] arr = cipher.split("");
+        for(int i=code;i<=cipher.length();i+=code){
+            answer += arr[i-1];
+        }
+        return answer;
+    }
+}
